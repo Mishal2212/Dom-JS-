d4f5fd4111ebd27e8cadf6927a5aab5b5f99ef85
Mishal Kesaria <kesariamishal@gmail.com>
2026-02-04 18:27:13 +0530

    final ipl gussing game

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
-var arr=[
+var arr = [
     {
-        team:"CSK",
-        primary:"Yellow",
-        secondary:"blue"
+        team: "CSK",
+        primary: "#F9CD05",   // rich yellow
+        secondary: "#1C1C1C", // charcoal black
+        captain: "Ruturaj Gaikwad",
+        trophies: 5
     },
     {
-        team:"RCB",
-        primary:"red",
-        secondary:"Black"
+        team: "RCB",
+        primary: "#D71920",   // bold red
+        secondary: "#111111", // deep black
+        captain: "Rajat Patidar",
+        trophies: 1
     },
     {
-        team:"DC",
-        primary:"blue",
-        secondary:"black"
+        team: "DC",
+        primary: "#17479E",   // deep royal blue
+        secondary: "#E11C2A", // sharp red
+        captain: "Rishabh Pant",
+        trophies: 0
     },
     {
-        team:"MI",
-        primary:"blue",
-        secondary:"gold"
+        team: "MI",
+        primary: "#004BA0",   // iconic MI blue
+        secondary: "#D4AF37", // metallic gold
+        captain: "Hardik Pandya",
+        trophies: 5
     },
     {
-        team:"KKR",
-        primary:"purple",
-        secondary:"gold"
+        team: "KKR",
+        primary: "#3A225D",   // dark purple
+        secondary: "#F3C300", // bright gold
+        captain: "Shreyas Iyer",
+        trophies: 2
     },
     {
-        team:"SRH",
-        primary:"orange",
-        secondary:"black"
+        team: "SRH",
+        primary: "#FB643E",   // vibrant orange
+        secondary: "#1A1A1A", // soft black
+        captain: "Pat Cummins",
+        trophies: 1
     },
     {
-        team:"PBKS",
-        primary:"red",
-        secondary:"white"
+        team: "PBKS",
+        primary: "#C41E3A",   // strong red
+        secondary: "#BFC0C0", // clean silver
+        captain: "Shikhar Dhawan",
+        trophies: 0
     }
-]
+];
+
+
 
 
 var btn=document.querySelector("button");
@@ -44,8 +60,9 @@ var h1=document.querySelector("h1");
 
 btn.addEventListener("click",function(){
     var num=Math.floor(Math.random()*arr.length);
-    h1.innerHTML=arr[num].team
+    h1.innerHTML=arr[num].team+"<br>Captain: "+arr[num].captain+"<br>Trophies won: "+arr[num].trophies;
     box.style.backgroundColor=arr[num].primary;
     main.style.backgroundColor=arr[num].secondary;
     
 })
+
