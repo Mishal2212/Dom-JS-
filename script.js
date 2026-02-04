f3fb7207d74eeaa5d2bf63d95992e96fa9a7e000
Mishal Kesaria <kesariamishal@gmail.com>
2026-02-04 19:30:58 +0530

    create element1

--- script.js
+++ script.js
@@ -1,68 +1,25 @@
-var arr = [
-    {
-        team: "CSK",
-        primary: "#F9CD05",   // rich yellow
-        secondary: "#1C1C1C", // charcoal black
-        captain: "Ruturaj Gaikwad",
-        trophies: 5
-    },
-    {
-        team: "RCB",
-        primary: "#D71920",   // bold red
-        secondary: "#111111", // deep black
-        captain: "Rajat Patidar",
-        trophies: 1
-    },
-    {
-        team: "DC",
-        primary: "#17479E",   // deep royal blue
-        secondary: "#E11C2A", // sharp red
-        captain: "Rishabh Pant",
-        trophies: 0
-    },
-    {
-        team: "MI",
-        primary: "#004BA0",   // iconic MI blue
-        secondary: "#D4AF37", // metallic gold
-        captain: "Hardik Pandya",
-        trophies: 5
-    },
-    {
-        team: "KKR",
-        primary: "#3A225D",   // dark purple
-        secondary: "#F3C300", // bright gold
-        captain: "Shreyas Iyer",
-        trophies: 2
-    },
-    {
-        team: "SRH",
-        primary: "#FB643E",   // vibrant orange
-        secondary: "#1A1A1A", // soft black
-        captain: "Pat Cummins",
-        trophies: 1
-    },
-    {
-        team: "PBKS",
-        primary: "#C41E3A",   // strong red
-        secondary: "#BFC0C0", // clean silver
-        captain: "Shikhar Dhawan",
-        trophies: 0
-    }
-];
-
-
-
-
 var btn=document.querySelector("button");
-var box=document.querySelector("#box");
 var main=document.querySelector("main");
-var h1=document.querySelector("h1");
+
 
 btn.addEventListener("click",function(){
-    var num=Math.floor(Math.random()*arr.length);
-    h1.innerHTML=arr[num].team+"<br>Captain: "+arr[num].captain+"<br>Trophies won: "+arr[num].trophies;
-    box.style.backgroundColor=arr[num].primary;
-    main.style.backgroundColor=arr[num].secondary;
-    
-})
+    var div=document.createElement("div");  
 
+    var x=Math.floor(Math.random()*100);
+    var y=Math.floor(Math.random()*100);
+    var r=Math.floor(Math.random()*100);
+
+    var c1=Math.floor(Math.random()*256);
+    var c2=Math.floor(Math.random()*256);
+    var c3=Math.floor(Math.random()*256);
+
+    div.style.height="100px";
+    div.style.width="100px";
+    div.style.position="absolute";
+    div.style.left=`${x}%`;
+    div.style.top=`${y}%`;
+    div.style.rotate=`${r}deg`;
+    div.style.backgroundColor=`rgb(${c1},${c2},${c3})`;
+
+    main.appendChild(div);
+})
