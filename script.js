2abbdfdaae4bb99c74652433c620394044609100
Mishal Kesaria <kesariamishal@gmail.com>
2026-02-04 19:45:48 +0530

    random quotes

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
+var quotes=[ "Keep going.",
+  "Code. Learn. Repeat.",
+  "Small steps daily.",
+  "Debug, don't quit.",
+  "Dream big.",
+  "Stay consistent.",
+  "Errors teach best.",
+  "Think. Code.",
+  "Focus matters.",
+  "Progress > perfection."
+];
+
 var btn=document.querySelector("button");
 var main=document.querySelector("main");
 
-
 btn.addEventListener("click",function(){
-    var div=document.createElement("div");  
 
-    var x=Math.floor(Math.random()*100);
-    var y=Math.floor(Math.random()*100);
-    var r=Math.floor(Math.random()*100);
 
-    var c1=Math.floor(Math.random()*256);
-    var c2=Math.floor(Math.random()*256);
-    var c3=Math.floor(Math.random()*256);
+    var a=Math.floor(Math.random()*quotes.length);
+    let h1=document.createElement("h1");
+    main.appendChild(h1);
 
-    div.style.height="100px";
-    div.style.width="100px";
-    div.style.position="absolute";
-    div.style.left=`${x}%`;
-    div.style.top=`${y}%`;
-    div.style.rotate=`${r}deg`;
-    div.style.backgroundColor=`rgb(${c1},${c2},${c3})`;
+    h1.style.fontSize="30px";
+    h1.style.position="absolute";
+    h1.style.top=Math.floor(Math.random()*90)+"%";
+    h1.style.left=Math.floor(Math.random()*90)+"%";
+    h1.style.transform="translate(-50%,-50%)";
+    h1.style.color="hsl("+Math.floor(Math.random()*360)+",100%,50%)";
+    h1.style.scale=Math.floor(Math.random()*4);
+    h1.innerText=quotes[a];
 
-    main.appendChild(div);
+
+
 })
