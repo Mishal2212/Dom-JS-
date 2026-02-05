41cc54cb7abe7e9aefce9a225ac9b13b93c60ddc
Mishal Kesaria <kesariamishal@gmail.com>
2026-02-05 19:22:46 +0530

    timeout downloadeffect

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-var quotes=[ "Keep going.",
-  "Code. Learn. Repeat.",
-  "Small steps daily.",
-  "Debug, don't quit.",
-  "Dream big.",
-  "Stay consistent.",
-  "Errors teach best.",
-  "Think. Code.",
-  "Focus matters.",
-  "Progress > perfection."
-];
-
+var grow=0;
 var btn=document.querySelector("button");
-var main=document.querySelector("main");
+var h2=document.querySelector("h2");
+var inner=document.querySelector(".inner");
 
 btn.addEventListener("click",function(){
+  
+  btn.style.pointerEvents="none";
+  var num=50 + Math.floor(Math.random()*50);
+
+  console.log(`your file will be downloaded in ${num/10} seconds`);
+
+  var int=setInterval(function(){
+    grow++;
+    h2.innerHTML=grow+"%";
+    inner.style.width=grow+"%"
+  },num)
+
+  setTimeout(function(){
+    clearInterval(int);
+    btn.innerHTML="Downloaded";
+    btn.style.opacity=0.5;
+  },num*100)
 
 
-    var a=Math.floor(Math.random()*quotes.length);
-    let h1=document.createElement("h1");
-    main.appendChild(h1);
-
-    h1.style.fontSize="30px";
-    h1.style.position="absolute";
-    h1.style.top=Math.floor(Math.random()*90)+"%";
-    h1.style.left=Math.floor(Math.random()*90)+"%";
-    h1.style.transform="translate(-50%,-50%)";
-    h1.style.color="hsl("+Math.floor(Math.random()*360)+",100%,50%)";
-    h1.style.scale=Math.floor(Math.random()*4);
-    h1.style.rotate=Math.floor(Math.random()*360)+"deg";
-    h1.innerText=quotes[a];
 })
