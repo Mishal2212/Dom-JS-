76c703ef220bf1cef613a23d99ee42df4b81beaf
Mishal Kesaria <kesariamishal@gmail.com>
2026-02-17 18:36:08 +0530

    queryselectorall

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-const keys = document.querySelectorAll(".key");
-
-// sab keys ko audio number assign kar do (28 → 63)
-keys.forEach((key, index) => {
-  key.dataset.audio = 28 + index;
-});
-
-document.addEventListener("keydown", function(e) {
-  let pressedKey = e.key.toUpperCase();
-  let el = document.getElementById(pressedKey);
-
-  if(el){
-    // visual press
-    el.style.transform = "scale(0.95)";
-    el.style.background = "orange";
-
-    // audio play
-    let num = el.dataset.audio;
-    let audio = new Audio(`audio/${num}.mp3`);
-    audio.currentTime = 0;
-    audio.play();
-  }
-});
-
-document.addEventListener("keyup", function(e) {
-  let pressedKey = e.key.toUpperCase();
-  let el = document.getElementById(pressedKey);
-
-  if(el){
-    el.style.transform = "scale(1)";
-
-    if(el.classList.contains("black")){
-      el.style.background = "black";
-    } else {
-      el.style.background = "white";
+var allelems=document.querySelectorAll(".elem")
+allelems.forEach(function(elem){
+  elem.childNodes[3].addEventListener('click',function(){
+    if(elem.childNodes[3].innerHTML=="Add friend"){
+      elem.childNodes[3].innerHTML="Remove friend"
     }
-  }
-});
+    else{
+      elem.childNodes[3].innerHTML="Add friend"
+    
+    }
+  })
+})
